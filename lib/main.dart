e91eefc5e460f5da9bcc495884782842c559c31e
khushimehta12 <58066257+khushimehta12@users.noreply.github.com>
2020-08-14 13:17:07 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String videoURL = "https://www.youtube.com/channel/UCOUJ-VCLgb70TVn1R4T4QWQ.";
+
+  YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+
+    _controller = YoutubePlayerController(
+        initialVideoId: YoutubePlayer.convertUrlToId(videoURL)
+    );
+
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Youtube Player"),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+
+              YoutubePlayer(
+                controller: _controller,
+                showVideoProgressIndicator: true,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
